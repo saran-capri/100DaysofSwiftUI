3cb8e636cae21231225dcb1982c0d7caacbfc2a8
saran <sarancapri@gmail.com>
2025-07-31 22:42:26 -0400

    Initial commit with 100 folders

---
+++ Day001/Day1.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+let number = 0.4 + 0.6
+print(number)
+
+let a = 10
+let b = 0.5
+print (Double(a) + b)
+
+var name = "John"
+name = "Jane"
+var rates = 56
+rates *= 2
+print(    rates)
