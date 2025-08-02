1646912cbca490a569daff2c982c0aa6a271062b
saran <sarancapri@gmail.com>
2025-08-01 23:27:35 -0400

    Booleans, string interpolation, and checkpoint 1

---
+++ Day002/string interpolation.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var greeting = "Hello, playground"
+let people = "Haters"
+let action = "hate"
+let lyric = people + " gonna " + action
+print(lyric)
+
+
+let name = "Taylor"
+let age = 26
+let message = "Hello, my name is \(name) and I'm \(age) years old."
+print(message)
